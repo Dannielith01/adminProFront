750d320e71cc296f553dc0d6363b01fd6eca803d
Daniel Zavala <Daniel.ZavalaH@gmail.com>
2018-03-27 20:12:30 -0700

    Se agrega login

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,5 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { NgForm } from '@angular/forms';
+
+import { UserService } from '../services/service.index';
+import { User } from '../models/user.model';
 
 declare function initPlugins();
 
@@ -10,14 +14,39 @@ declare function initPlugins();
 })
 export class LoginComponent implements OnInit {
 
-  constructor( public router: Router) { }
+  email: string;
+  remember: boolean;
+
+  constructor(
+    public router: Router,
+    public userService: UserService
+  ) { }
 
   ngOnInit() {
     initPlugins();
+
+    if (localStorage.getItem('email')) {
+      this.remember = true;
+    }
+
+    this.email = localStorage.getItem('email') || '';
   }
 
-  login() {
-    this.router.navigate( ['dashboard'] );
+  login( form: NgForm ) {
+    if (form.invalid) {
+      return;
+    }
+
+    const user = new User(
+      null,
+      null,
+      form.value.email,
+      form.value.pass,
+    );
+
+    this.userService.login( user, form.value.remember)
+        .subscribe( () => this.router.navigate(['/dashboard']) );
+
   }
 
 }
--- src/app/services/user/user.service.ts
+++ src/app/services/user/user.service.ts
@@ -9,6 +9,23 @@ export class UserService {
 
   constructor(public http: HttpClient) { }
 
+  login( user: User, remember: boolean = false ) {
+    const url = URL_SERVICES + '/login';
+
+    if (remember) {
+      localStorage.setItem( 'email', user.email );
+    } else {
+      localStorage.removeItem( 'email' );
+    }
+
+    return this.http.post( url, user )
+                .map((resp: any) => {
+                  localStorage.setItem( 'id', resp.id );
+                  localStorage.setItem( 'token', resp.token );
+                  localStorage.setItem( 'user', JSON.stringify( resp.token ) );
+                });
+  }
+
   createUser(user: User) {
     const url = URL_SERVICES + '/user';
 
